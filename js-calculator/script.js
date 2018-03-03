01771885da653182c34087ae14df0cf53cf0d3b6
antoinevo <antoine.monarch@gmail.com>
2018-03-02 20:12:12 -0800

    revert to dbe7d15b36936eb8813d404e025159467213e4ac

---
+++ js-calculator/script.js
@@ -0,0 +1,63 @@
+
+var input = firstNumber = operator = "";
+document.getElementById('calculator').addEventListener('click', handleClick);
+
+function handleClick(event){
+	const id = event.target.id;
+	if (id == 1 || id == 2 || id == 3 || id == 4 || 
+		id == 5 || id == 6 || id == 7 || id ==8 || 
+		id ==9 || id == 0){
+		updateDisplay(event.target.innerHTML);
+	}
+	if (id === "AC"){
+		clearDisplay();
+	}
+	if (id === "+" || id === "-" || id === "*" || id === "/"){
+		applyOperator(event.target.innerHTML);
+	}
+	if (id === "="){
+		calculate();
+	}
+}
+
+
+
+function updateDisplay(buttonInput){
+	input += buttonInput;
+   document.getElementById("display").textContent = input;
+}
+
+function clearDisplay(){
+	input = "";
+	document.getElementById("display").textContent = input;
+}
+
+function applyOperator(operatorInput){
+	operator = operatorInput;
+	firstNumber = input;
+	input = "";
+	document.getElementById("display").textContent = input;
+}
+
+function calculate(){
+var result = "";
+	switch(operator){
+		case '+':
+			result = (+firstNumber + +input);
+			break;
+		case '-':
+			result = (firstNumber - input);
+			break;
+		case '*':
+			result = (firstNumber * input);
+			break;
+		case '/':
+			result = (firstNumber / input);
+			break;
+	}
+input = result;
+document.getElementById("display").textContent = input;
+
+}
+
+
