62c9b247fe6c00b5d6935b6fe0e978782aa127a1
antoinevo <antoine.monarch@gmail.com>
2018-03-17 16:28:38 -0700

    reducers

--- todo-redux-app/src/containers/App.js
+++ todo-redux-app/src/containers/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+	
   render() {
     return (
       <div> 
-        
+        hey
       </div>
     )
   }
