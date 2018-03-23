710026dd261be24ad8f718e7328833802229a326
antoinevo <antoine.monarch@gmail.com>
2018-03-22 23:55:49 -0700

    added an action

--- todo-redux-app/src/actions/index.js
+++ todo-redux-app/src/actions/index.js
@@ -1,3 +1,4 @@
 
 export const addTodo = text => ({ type: 'ADD_TODO', text});
 export const deleteTodo = id => ({ type: 'DELETE_TODO', id });
+export const toggleTodo = id => ({ type: 'TOGGLE_TODO', id });
